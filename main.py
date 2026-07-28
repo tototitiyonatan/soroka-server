e5d837dc263ae6749a730e02a8c647dc33223770
tototitiyonatan <numreggaeman@gmail.com>
2026-07-28 23:24:44 +0300

    Migrate to persistent PostgreSQL database

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ import os
 
 
 # ----------------- 1. הגדרת בסיס הנתונים (SQLAlchemy) -----------------
-SQLALCHEMY_DATABASE_URL = "sqlite:///./soroka_staff.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+import os
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./soroka_staff.db")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
